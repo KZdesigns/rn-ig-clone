205cf2836a439fd30dc11032e4fba168e2dc80e5
Kyle Zimmerman <kzdesignsios@gmail.com>
2023-04-05 23:37:02 -0700

    I think the issue is that I am doing to much in my photo actions, we should refactor the the action to just get all the objects and store them then we can loop through the information needed on the component to make things faster? maybe

--- store/photoActions.js
+++ store/photoActions.js
@@ -1,21 +1,28 @@
 import { saveFile } from "./photoSlice";
 import getDatabaseObj from "../util/getDatabaseObj";
+import { getDatabase, ref, onValue } from "firebase/database";
 
 export const fetchPhotoListData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
-      const data = await getDatabaseObj();
-      const dataKeys = Object.keys(data);
-      // const dataArray = [data];
-
-      const imageUrls = dataKeys.map((key, index) => {
-        return data[key].imageUri;
-      });
+      const db = getDatabase();
+      const starCountRef = ref(db, "/images");
+      const imageUrls = [];
+      await Promise.all(
+        onValue(starCountRef, (snapshot) => {
+          const data = snapshot.val();
+          const keys = Object.keys(data);
+          keys.forEach((element) => {
+            imageUrls.push(data[element].imageUri);
+          });
+        })
+      );
       return imageUrls;
     };
 
     try {
       const photoData = await fetchData();
+      console.log(photoData);
       dispatch(
         saveFile({
           imageUrls: photoData,
